b208ff2652a9f6fb7577642d6370629e531da14e
emmendezp <emmendezp@users.noreply.github.com>
2022-08-31 03:17:06 +0000

    deploy: f49a1578f2785a741e13414572c61fc57a282ac3

--- sketches/video-dom.js
+++ sketches/video-dom.js
@@ -3,7 +3,7 @@ function setup() {
   noCanvas();
 
   vid = createVideo(
-    ['/showcase/sketches/fingers.mov', '/showcase/sketches/fingers.webm'],
+    ['/RGB/sketches/fingers.mov', '/RGB/sketches/fingers.webm'],
     vidLoad
   );
 
